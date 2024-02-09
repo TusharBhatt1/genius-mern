a10c9f549043a0da6e5ebca8627d82fa833b26df
Tushar Bhatt <tusharbhatt0135@gmail.com>
2024-02-09 16:27:18 +0530

    loading

--- frontend/src/pages/CodeGeneration.tsx
+++ frontend/src/pages/CodeGeneration.tsx
@@ -13,7 +13,7 @@ export interface ChatProps {
 
 export default function CodeGeneration() {
   const [prompt, setPrompt] = useState("");
-  const [isProcessing, setIsProcessing] = useState(false);
+  const [isProcessing, setIsProcessing] = useState(true);
   const [isFetchingChats, setIsFetchingChats] = useState(false);
   const [showCopiedText, setCopiedText] = useState(false);
   const [isError, setIsError] = useState(false);
